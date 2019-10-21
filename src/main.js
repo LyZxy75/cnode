3cb8ad8443dfe8bce48017b4941efa968032f7fc
unknown <1010006459@qq.com>
2019-10-21 14:24:32 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,9 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+import store from './store/index'
+import axios from 'axios'
+import http from './http'
 
+
+Vue.prototype.$axios = http
 Vue.config.productionTip = false
+
+// 响应拦截
+axios.defaults.timeout = 20000 // 设置超时时间
+// 过滤请求结果 只返回结果的data
+axios.interceptors.response.use(
+    response => {
+      return response.data
+    }, error => {
+      return Promise.reject(error)
+    })
+
 
 new Vue({
   router,
